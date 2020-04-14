891c65cdae6191dbcc68f633214a6771fe4f0fd4
Roberto D'Amico <rob@orangejellyfish.com>
2020-04-14 18:47:09 +0100

    Linting fix

--- src/utils/run-warm/index.js
+++ src/utils/run-warm/index.js
@@ -2,7 +2,7 @@
 // Lambda handler functions in this higher-order function we can keep the
 // container running our code alive which reduces the chances of slow cold
 // starts, thereby improving the performance of our Lambdas.
-const runWarm = handler => async (event, ...rest) => {
+const runWarm = (handler) => async (event, ...rest) => {
   if (event.source === 'aws.events') {
     return 'pinged';
   }
