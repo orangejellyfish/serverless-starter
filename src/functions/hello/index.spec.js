763f702e02fe1a9a4907e9fee0c89dc29892a181
Roberto D'Amico <rob@orangejellyfish.com>
2020-04-15 08:50:12 +0100

    Update unit test for hello function
    Inline usage of the runWarm wrapper requires a slight change to the unit test, passing an event object. This is used by the wrapper to determine if the event source is aws.
    
    This was not an issue before as it was used on the default export which had no test coverage.

--- src/functions/hello/index.spec.js
+++ src/functions/hello/index.spec.js
@@ -2,6 +2,6 @@ import { hello } from '.';
 
 describe('[Function: hello]', () => {
   it('should return the expected response', async () => {
-    expect(await hello()).toMatchSnapshot();
+    expect(await hello({})).toMatchSnapshot();
   });
 });
