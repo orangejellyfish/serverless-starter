92765cd86694d7d1ac7b6921f5436d654f1a1e05
Roberto D'Amico <rd@Robertos-MacBook-Pro.local>
2020-04-17 10:49:09 +0100

    Update hello function
    Rolling back change made to function declaration now that  it;s not needed.

--- src/functions/hello/index.js
+++ src/functions/hello/index.js
@@ -2,7 +2,7 @@ import runWarm from '../../utils/run-warm';
 
 // Lambda handler. We are using an async function to simplify the code and
 // remove the need to use a callback.
-export const hello = async (event) => {
+export const hello = async function (event) {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
