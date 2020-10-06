460b05a8749e15f8a2161fab31ccdf04710a90e9
James Allardice <james@allardice.me>
2020-10-06 07:50:28 +0100

    Include Node version in default function response
    This makes it easy to check the version of Node running in the Lambda
    which is useful to ensure your local dev environment matches the remote
    environment.

--- src/functions/hello/index.js
+++ src/functions/hello/index.js
@@ -8,6 +8,7 @@ export const hello = async function hello(event) {
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
+      version: process.version,
     }),
   };
 
